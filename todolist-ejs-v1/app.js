2c0defe07b55178e994badc2b7fb3f5507578429
James Leakos <jamesleakos@gmail.com>
2022-06-01 18:58:52 -0700

    some more work on todo list

--- todolist-ejs-v1/app.js
+++ todolist-ejs-v1/app.js
@@ -3,6 +3,8 @@ const https = require("https");
 
 const app = express();
 
+var items = [];
+
 // use EJS
 app.set("view engine", "ejs");
 
@@ -15,38 +17,22 @@ app.use(express.static("public"));
 // get method for home route
 app.get("/", function (req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
-
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to " + currentDay);
-      break;
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
   }
+  var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {kindOfDay: day});
+  res.render("list", {kindOfDay: day, newListItems: items});
 });
+
+// post method for home route
+app.post("/newItem", function(req, res) {
+  item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+})
 
 // listen
 app.listen(3000, function () {
