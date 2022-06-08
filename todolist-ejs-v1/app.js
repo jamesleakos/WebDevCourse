47e104899b7a0e3b0111b6dad5d5ea4cec28d7f3
James Leakos <jamesleakos@gmail.com>
2022-06-08 14:41:48 -0400

    work on the todo site

--- todolist-ejs-v1/app.js
+++ todolist-ejs-v1/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const https = require("https");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-var items = [];
+let items = [];
+let workItems = [];
 
 // use EJS
 app.set("view engine", "ejs");
@@ -16,23 +18,39 @@ app.use(express.static("public"));
 
 // get method for home route
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  }
-  var day = today.toLocaleDateString("en-US", options);
-
-  res.render("list", {kindOfDay: day, newListItems: items});
+  let day = date.getDay();
+  res.render("list", {listTitle: day, newListItems: items});
 });
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work", newListItems: workItems});
+});
+
+app.post("/work", function(req, res) {
+  item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
 
 // post method for home route
 app.post("/newItem", function(req, res) {
-  item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+
+  } else {
+    items.push(item);
+    res.redirect("/");
+
+  }
 })
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+
 
 // listen
 app.listen(3000, function () {
